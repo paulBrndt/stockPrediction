7e2103aa7d947502b9ba2214230407532f903800
Paul Brendtner <einsaetze_finanzen.0r@icloud.com>
2024-07-08 18:30:46 +0200

    little update

--- predModel.py
+++ predModel.py
@@ -12,6 +12,7 @@ from sklearn.neural_network import MLPRegressor
 
 from utils import printNothing, infoMsg, Data
 
+import datetime
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -22,7 +23,7 @@ load_dotenv()
 MODELS = [SVC, LogisticRegression, MLPRegressor]
 
 class StockPredModel():
-    def __init__(self, data, pathToSaved: str = "", loadAtBeginning: bool = False, msg: Callable = None) -> None:
+    def __init__(self, data, pathToSaved: str = "model.pkl", loadAtBeginning: bool = True, msg: Callable = None) -> None:
 
         self.data: list[Data]       = []
         self.msg                    = msg       or printNothing
@@ -30,8 +31,9 @@ class StockPredModel():
         self.scaler                 = StandardScaler()
 
         if pathToSaved:
+            self.pathToSaved = pathToSaved
             if loadAtBeginning:
-                self.load(pathToSaved)
+                self.load()
         else:
             self.pathToSaved = "model.pkl"
 
@@ -56,7 +58,7 @@ class StockPredModel():
         self.xTrain, self.xTest, self.yTrain, self.yTest = train_test_split(allFeatures, allTargets, test_size=testSize)
 
     def train(self):
-        self.msg("training")
+        self.msg("trainiert")
         self.model.fit(self.xTrain, self.yTrain)
         self.getAccuracy()
         if self.accuracy > 0.5:
@@ -69,13 +71,12 @@ class StockPredModel():
     def predict(self, data):
         return self.model.predict(data)
     
-    def save(self, filePath: str = ""):
-        self.pathToSaved = filePath if filePath else self.pathToSaved
+    def save(self):
         with open(f"models/{self.pathToSaved}", "wb") as f:
+            print(self.pathToSaved)
             pk.dump(self, f)
 
-    def load(self, filePath: str = ""):
-        self.pathToSaved = filePath if filePath else self.pathToSaved
+    def load(self):
         with open(f"models/{self.pathToSaved}", "rb") as f:
             self.__dict__.update(pk.load(f).__dict__)
             self.train()
@@ -83,17 +84,18 @@ class StockPredModel():
     
 files = os.environ.get("stock_array").replace("'", "").replace(" ", "").split(",")                
 
-model = StockPredModel(os.environ.get("stock_array").replace("'", "").replace(" ", "").split(",")[:5], msg=infoMsg)
+model = StockPredModel(files[:5], msg=infoMsg)
 print(model.accuracy)
 model.train()
-import datetime
+print(model.predict(np.array([[0.4, -0.7600002289, 0.0, 0.0]])))
 oldTime = datetime.datetime.now()
 for item in files:
-    print(files.index(item))
+    print(f"Verarbeitung von Datei Nummer {files.index(item)} mit dem Namen {item}")
     model.addData(item)
-    print(model.accuracy)
+    print(f"Wahrscheinlichkeit nach Training bei Testdaten richtig zu liegen: {model.accuracy: .3f}")
     model.train()
     model.save()
     time = datetime.datetime.now()
-    print(time - oldTime)
+    print(f"Benötigte Zeit um mit Daten zu trainieren: {time - oldTime}")
     oldTime = time
+    print("Um das Prgramm zu stoppen, tippe in das Terminal und drücke ^c")
